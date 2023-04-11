d8bcc3d3b5a744ca455abde57d199ad8691eda6c
LBeukennoot <lars@beukenkamp.net>
2023-04-11 17:28:31 +0200

    Added countdown backend

--- src/Scenes/Startup.ts
+++ src/Scenes/Startup.ts
@@ -1,6 +1,8 @@
 import Icerink from "./Icerink";
 
 export default class Startup extends Phaser.Scene {
+    timedEvent: Phaser.Time.TimerEvent;
+    countdownText: Phaser.GameObjects.Text;
 
     constructor() {
         super("startup")
@@ -11,10 +13,18 @@ export default class Startup extends Phaser.Scene {
     }
 
     create() {
-        this.scene.start("icerink")
+        this.countdownText = this.add.text(0,0, "")
+
+        this.timedEvent = this.time.delayedCall(3000, this.onEvent, [], this);
+
     }
 
     update() {
+        this.countdownText.setText(Math.floor((this.timedEvent.getProgress() * 3) + 1).toString())
+    }
+
+    onEvent() {
+        this.scene.start("icerink")
 
     }
 }
